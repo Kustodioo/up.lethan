59fe52fc97fbaae11d495c6417082ac416571ade
douglaskustodio <129959703+douglaskustodio@users.noreply.github.com>
2024-08-07 11:48:14 -0400

    v

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.js';  // Importa o App como padrão
+import App from './App.js';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
